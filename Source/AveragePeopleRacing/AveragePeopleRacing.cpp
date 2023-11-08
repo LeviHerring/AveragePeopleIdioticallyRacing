e28cb54269a0df0f3bd000e49826bc2b44b31233
DESKTOP-ONOO3MD\theob <leviherring16@gmail.com>
2023-11-08 20:58:04 +0000

    First Commit

---
+++ Source/AveragePeopleRacing/AveragePeopleRacing.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AveragePeopleRacing.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AveragePeopleRacing, "AveragePeopleRacing" );
+ 
