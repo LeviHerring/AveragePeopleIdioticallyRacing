951855942c4bccea2f5acf71ba66ed80f5fbcb37
DESKTOP-ONOO3MD\theob <leviherring16@gmail.com>
2023-11-16 15:54:10 +0000

    Made changes to pawn, still doesn't move

--- Source/AveragePeopleRacing/Private/MyPawn_PlayerCar.cpp
+++ Source/AveragePeopleRacing/Private/MyPawn_PlayerCar.cpp
@@ -2,6 +2,7 @@
 
 
 #include "MyPawn_PlayerCar.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "EnhancedInput/Public/EnhancedInputSubsystems.h"
 #include "EnhancedInput/Public/EnhancedInputComponent.h"
 
@@ -17,50 +18,23 @@ AMyPawn_PlayerCar::AMyPawn_PlayerCar()
 
 void AMyPawn_PlayerCar::PawnMove(const FInputActionValue& Value)
 {
-	const float TopSpeed = 5.0f; 
-	float Speed = 0.0f; 
-    const FVector2D MoveValue = Value.Get<FVector2D>();
-    const FRotator MovementRotation(0, Controller->GetControlRotation().Yaw, 0);
-	const FVector2D MoveAxis = Value.Get<FVector2D>(); 
-//	const FRotator moveRotation(0.0f, Controller->GetControlRotation().Yaw, 0.0f); 
-//	const FVector directionVector = moveRotation.RotateVector(FVector::RightVector);
-//	AddMovementInput(directionVector, MoveAxis.X);
-////If the movement is in the forward or backward direction.MoveAxis.Y > 0.05f || MoveAxis.Y < -0.05f)
-//	const FVector directionVector = moveRotation.RotateVector(FVector::ForwardVector);
-//	AddMovementInput(directionVector, MoveAxis.Y);
-	FVector Location = GetActorLocation();
-	if (MoveAxis.X == 1)
-	{
-		if (Speed < TopSpeed)
-		{
-				Speed += 0.0000001; 
-				Location.X += MoveAxis.X + Speed;
-				//set timer or something 
-		}
-		else 
-		{
-			Speed = 5.0f;
-			Location.X += MoveAxis.X + Speed; 
-		}
-	}
-	if (MoveAxis.Y == -1)
-	{
-		Speed = 0.0f; 
-		if (Speed > -TopSpeed)
-		{
-				Speed -= 0.0000001;
-				Location.Y += MoveAxis.Y + Speed;
+	
+	const FVector2D MoveVector = Value.Get<FVector2D>();
+	const FRotator MoveRotation(0.0f, Controller->GetControlRotation().Yaw, 0.0f);
 
-		}
-		else
+	if (MoveVector.X > 0.05f || MoveVector.X < -0.05f)
+	{
+		
+		
+		const FVector DirectionVector = MoveRotation.RotateVector(FVector::RightVector) * 10.0f;
+		AddMovementInput(DirectionVector, MoveVector.X);
+
+		if (MoveVector.X > 0.05f)
 		{
-			Speed = -5.0f;
-			Location.Y += MoveAxis.Y + Speed;
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, TEXT("Input Forward" + DirectionVector.ToString()));
 		}
-	}
-	Location.X += MoveAxis.X;
-	Location.Y += MoveAxis.Y;
-	SetActorLocation(Location); 
+	} 
+	
 
 }
 
